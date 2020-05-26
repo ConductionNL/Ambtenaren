58ac09ce5491470deace433142e10a4db501829c
Robert Zondervan <rjzondervan@gmail.com>
2020-05-26 13:19:02 +0200

    Up

--- api/src/Entity/JobFunction.php
+++ api/src/Entity/JobFunction.php
@@ -27,7 +27,7 @@ use Ramsey\Uuid\UuidInterface;
  * )
  *
  * @ORM\Entity(repositoryClass="App\Repository\JobFunctionRepository")
- * @Gedmo\Loggable(logEntryClass="App\Entity\ChangeLog")
+ * @Gedmo\Loggable(logEntryClass="Conduction\CommonGroundBundle\Entity\ChangeLog")
  *
  * @ApiFilter(BooleanFilter::class)
  * @ApiFilter(OrderFilter::class)
