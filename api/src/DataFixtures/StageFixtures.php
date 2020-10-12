e012b715604a07ac68e898514dda98871eca0f5b
Wilco Louwerse <wilco@conduction.nl>
2020-10-12 13:47:46 +0200

    Renamed conductionFixtures to StageFixtures

--- api/src/DataFixtures/StageFixtures.php
+++ api/src/DataFixtures/StageFixtures.php
@@ -29,7 +29,8 @@ class ConductionFixtures extends Fixture
         if (
             !$this->params->get('app_build_all_fixtures') &&
             $this->params->get('app_domain') != 'zuiddrecht.nl' && strpos($this->params->get('app_domain'), 'zuiddrecht.nl') == false &&
-            $this->params->get('app_domain') != 'zuid-drecht.nl' && strpos($this->params->get('app_domain'), 'zuid-drecht.nl') == false
+            $this->params->get('app_domain') != 'zuid-drecht.nl' && strpos($this->params->get('app_domain'), 'zuid-drecht.nl') == false &&
+            $this->params->get('app_domain') != 'conduction.academy' && strpos($this->params->get('app_domain'), 'conduction.academy') == false
         ) {
             return false;
         }
